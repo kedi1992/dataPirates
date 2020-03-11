8e40a1ac19e5ddd73074f40525bb64e869ff495e
Sumer Raipurkar <sumer.r@corp.cumulus>
2020-03-11 15:42:35 +0530

    added api for lisiting all AWS services

--- aws/views.py
+++ aws/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
+import traceback
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 
 
 def index(request):
-    print("hello")
     return render(request, 'aws/index.html', context={})
+
+class GetServices(APIView):
+    def get(self, request):
+        try:
+            services_list = ["Amazon S3", "Amazon EC2 [Elastic Compute Cloud]", "AWS Lambda", "Amazon Glacier", "Amazon SNS", "Amazon CloudFront", "Amazon EBS [Elastic Block Store]", "Amazon Kinesis", "Amazon VPC", "Amazon SQS", " Amazon Elastic Beanstalk", "DynamoDB", "Amazon RDS [Relational Database Service]", "Amazon ElastiCache", "Amazon Redshift"]
+
+            return Response(services_list, status=200)
+        except Exception as e:
+            print("Exception in getServices view : {}".format(traceback.print_exc(e)))
+            return False
