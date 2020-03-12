7c8a593d97c2676f0902b6235a847abc65b385ae
PRAVIN KEDAR <pravin.kedar@cumulus-systems.com>
2020-03-12 18:01:07 +0530

    UI Changes

--- aws/urls.py
+++ aws/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from aws import views
 from django.conf.urls import url
 from aws.views import GetServices
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import re_path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+    # html page urls
     path('', views.index, name="index"),
+    path('ec2/', views.ec2, name="result"),
+    path('cartInfo/', views.cart_info, name="result"),
+    re_path(r'^category/', views.category, name="category"),
+    re_path(r'^result/', views.result, name="result"),
+    # api urls
     url(r'^aws/getServices', csrf_exempt(GetServices.as_view())),
-
 ]
--- aws/views.py
+++ aws/views.py
@@ -8,6 +8,45 @@ from rest_framework.views import APIView
 def index(request):
     return render(request, 'aws/index.html', context={})
 
+
+def cart_info(request):
+    return render(request, 'aws/cart_info.html', context={})
+
+
+def ec2(request):
+    return render(request, 'aws/vm.html', context={})
+
+
+def category(request):
+    category_name = str(request.path).split("/")[-1]
+    print("category_name : ", category_name)
+    service_name = request.GET.get('type')
+    return render(request, 'aws/{page_name}.html'.format(page_name=category_name), context={"serviceName": service_name})
+
+
+def result(request):
+    key = request.GET.get("key", None)
+    service_type = request.GET.get("type", None)
+    category_name = str(request.path).split("/")[-1].replace("type=", "")
+    info_list = []
+    tmp = str(request.path).split("/")[-2].replace("type=", "")
+    if "vm" == tmp:
+        # info_list.append(request.GET.get("type", None))
+        info_list.append("OS : " + request.GET.get("os_type", ""))
+        # info_list.append("CPU : " + request.GET.get("cpu", ""))
+        # info_list.append("RAM : " + request.GET.get("ram", ""))
+    elif "storage" == tmp:
+        info_list.append(request.GET.get("key", ""))
+    print("service_type :", service_type)
+    print("category_name : ", category_name)
+    print("key : ", key)
+    print("tmp : ", tmp)
+    print("info : ", info_list)
+    return render(request, 'aws/result.html', context={"type": service_type,
+                                                       "key": key, "category": category_name,
+                                                       "info": info_list})
+
+
 class GetServices(APIView):
     def get(self, request):
         try:
