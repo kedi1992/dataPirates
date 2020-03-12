40582e93fc0c0a4d3a81f334809d093bf9d94053
Sumer Raipurkar <sumer.r@corp.cumulus>
2020-03-12 12:50:58 +0530

    Added library for S3 and data json for all regions for S3 service

--- aws/views.py
+++ aws/views.py
@@ -2,18 +2,38 @@ from django.shortcuts import render
 import traceback
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import requests
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'aws/index.html', context={})
 
+
+class GetAllOfferCodes(APIView):
+    def get(self, request):
+        try:
+            all_offerCodes_url = "https://pricing.amazonaws.com/offers/v1.0/aws/index.json"
+            response = requests.get(url=all_offerCodes_url)
+            if response.status_code == 200:
+                all_offer_codes = response.json()
+                return Response(all_offer_codes, status=200)
+            else:
+                return Response("Something went wrong", status=400)
+        except Exception as e:
+            print("Exception in getRegionIndex view : {}".format(traceback.print_exc(e)))
+            return False
+
+
 class GetServices(APIView):
     def get(self, request):
         try:
-            services_list = ["Amazon S3", "Amazon EC2 [Elastic Compute Cloud]", "AWS Lambda", "Amazon Glacier", "Amazon SNS", "Amazon CloudFront", "Amazon EBS [Elastic Block Store]", "Amazon Kinesis", "Amazon VPC", "Amazon SQS", " Amazon Elastic Beanstalk", "DynamoDB", "Amazon RDS [Relational Database Service]", "Amazon ElastiCache", "Amazon Redshift"]
+            services_list = ["Amazon S3", "Amazon EC2 [Elastic Compute Cloud]", "AWS Lambda", "Amazon Glacier", "Amazon SNS", "Amazon CloudFront", "Amazon EBS [Elastic Block Store]", "Amazon Kinesis", "Amazon VPC", "Amazon SQS", "Amazon Elastic Beanstalk", "DynamoDB", "Amazon RDS [Relational Database Service]", "Amazon ElastiCache", "Amazon Redshift"]
 
             return Response(services_list, status=200)
         except Exception as e:
             print("Exception in getServices view : {}".format(traceback.print_exc(e)))
             return False
+
+
